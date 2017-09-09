6ac2344d2dc18f628148dbd34b8c7e497e225f02
Meri Karamyan <meri-qaramyan@mail.ru>
2017-09-09 13:53:49 +0500

    first commit

---
+++ homework2.js
@@ -0,0 +1,13 @@
+var number1 = 14;
+var number2 = 16;
+var result;
+var myName = "Meri";
+var currentYear = 2017, myBirthDate = 1994, myAge;
+
+result = number1 + number2;
+console.log(result);
+console.log(myName);
+
+myAge = currentYear - myBirthDate - 3;
+console.log(myAge);
+
